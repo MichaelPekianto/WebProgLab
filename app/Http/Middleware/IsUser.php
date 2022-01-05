db818676228718d2b6185ecffb476c8d1fd43aba
jechenn <85722794+jechenn@users.noreply.github.com>
2022-01-05 17:03:35 +0700

    middleware
    middleware kelar

---
+++ app/Http/Middleware/IsUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(empty(auth()->user()->id)){
+            return redirect()->intended('/');
+        }
+        else if(auth()->user()->id == 1){
+            return redirect()->intended('/');
+        }
+        else if(auth()->user()->id > 1){
+            return $next($request);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,15 @@ ROute::post('/logout', ([\App\Http\Controllers\LoginController::class, 'logout']
 Route::get('/register', ([\App\Http\Controllers\RegisterController::class, 'index']))->middleware('guest');
 Route::post('/register', ([\App\Http\Controllers\RegisterController::class, 'store']));
 
-Route::get('/updateprofile', ([\App\Http\Controllers\RegisterController::class, 'view']));
+Route::get('/updateprofile', ([\App\Http\Controllers\RegisterController::class, 'view']))->middleware('is_user');
 Route::post('/updateprofile', ([\App\Http\Controllers\RegisterController::class, 'update']));
 
 Route::get('/search', ([\App\Http\Controllers\ProductController::class, 'search']));
 
-Route::get('/insertproduct',([\App\Http\Controllers\ProductController::class, 'index']));
+Route::get('/insertproduct',([\App\Http\Controllers\ProductController::class, 'index']))->middleware('is_admin');
 Route::post('/insertproduct',([\App\Http\Controllers\ProductController::class, 'insertProduct']));
 
-Route::get('/updateproduct/{id}',([\App\Http\Controllers\ProductController::class, 'viewUpdate']));
+Route::get('/updateproduct/{id}',([\App\Http\Controllers\ProductController::class, 'viewUpdate']))->middleware('is_admin');
 Route::post('/updateproduct/{id}',([\App\Http\Controllers\ProductController::class, 'updateProduct']));
 
 Route::get('/', ([\App\Http\Controllers\ProductController::class, 'viewProduct']));
@@ -41,13 +41,13 @@ Route::get('/', ([\App\Http\Controllers\ProductController::class, 'viewProduct']
 Route::get('/details/{id}', ([\App\Http\Controllers\ProductController::class, 'details']));
 Route::post('/details/{id}',([\App\Http\Controllers\CartController::class, 'add']));
 
-Route::get('/manageuser', ([\App\Http\Controllers\RegisterController::class,'userData']));
+Route::get('/manageuser', ([\App\Http\Controllers\RegisterController::class,'userData']))->middleware('is_admin');
 Route::post('/manageuser/{id}', ([\App\Http\Controllers\RegisterController::class,'delete']));
 
-Route::get('/cart',([CartController::class, 'index']));
-Route::get('/deletecart/{id}',([CartController::class, 'deletecart']));
+Route::get('/cart',([CartController::class, 'index']))->middleware('is_user');
+Route::get('/deletecart/{id}',([CartController::class, 'deletecart']))->middleware('is_user');
 
-Route::get('/checkout',([CartController::class,'checkout']));
-Route::get('/transaction',([CartController::class,'transaction']));
+Route::get('/checkout',([CartController::class,'checkout']))->middleware('is_user');
+Route::get('/transaction',([CartController::class,'transaction']))->middleware('is_user');
 
-Route::get('transactiondetail/{id}',([CartController::class, 'detailtransaction']));
+Route::get('transactiondetail/{id}',([CartController::class, 'detailtransaction']))->middleware('is_user');
